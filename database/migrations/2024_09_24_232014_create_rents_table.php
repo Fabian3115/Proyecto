dbe99d3d5d21b49718e2877325386ea56d529a41
Fabian3115 <fabianestebantorresandrade123@gmail.com>
2024-09-24 18:25:38 -0500

    tables

---
+++ database/migrations/2024_09_24_232014_create_rents_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rents', function (Blueprint $table) {
+            $table->id();
+            $table->string('state');
+            $table->time('time_start');
+            $table->time('time_end');
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('bicycle_id')->constrained();
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rents');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
+
+        Route::get('/admin/dashboard', function (){
+            return view('Interfaz.dashboard_admin');
+        })->name('admin.dashboard')->middleware('is_admin');
+        Route::get('/user/dashboard', function (){
+            return view('Interfaz.dashboard_user');
+        })->name('user.dashboard');
+
+        
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
